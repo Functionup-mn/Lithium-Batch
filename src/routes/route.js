bc848ac66d59a486dc24a5531669fd5398b7e7c6
Functionup-mn <nadeem2mau@gmail.com>
2022-10-19 22:21:26 +0530

    assignment 2

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,22 +1,24 @@
 const express = require('express');
-core = require('underscore')
+const myHelper = require('../util/helper')
+const underscore = require('underscore')
 
 const router = express.Router();
 
-//Q1.
-// -write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7]     4 is missing
+// Q2.
+// -write an api which gives the missing number in an array of integers starting from anywhere….e.g [33,34, 35, 37, 38]: 36 is missing
 // Your route code will look like this
-router.get("/sol1", function (req, res) {
-//logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
-let arr= [1,2,4,5,6,7]
-let missingNumber
-///LOGIC WILL GO HERE
-let n = arr.length + 1
-let total = (n*(n+1))/2
-let sum = arr.reduce((a, b) => (a + b))
-missingNumber = total - sum
-
-res.send( { data: missingNumber } );
+router.get("/sol2", function (req, res) {
+    //logic : sum of n consecutive numbers is [ n * (first + last) / 2 ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+    let arr = [33, 34, 35, 37, 38]
+    let missingNumber
+    ///LOGIC WILL GO HERE
+    let n = arr.length + 1
+    let first = arr[0]
+    let last = arr[arr.length - 1]
+    let result = (n * (first + last)) / 2
+    let sum = arr.reduce((a, b) => (a + b))
+    missingNumber = result - sum
+    res.send({ data: missingNumber });
 });
 
 module.exports = router;
