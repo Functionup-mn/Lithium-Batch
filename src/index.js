91fa9f887cdd1fa67708f75266409c9a8f8442e1
Functionup-mn <nadeem2mau@gmail.com>
2022-10-25 10:38:28 +0530

    some changes here

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://functionup-cohort:3bMssIUqmBmIqb3e@cluster0.3ccgp0v.mongodb.net/PK-100?retryWrites=true&w=majority"
+mongoose.connect("mongodb+srv://mnadeem:h1QaS5NNUx2zm2tM@cluster0.v4od3qa.mongodb.net/newLithiumMN1-db"
 , {
    useNewUrlParser: true 
 }
