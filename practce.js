d974fc5311e87a1be6b14390fdd998f7967f4619
Functionup-mn <nadeem2mau@gmail.com>
2022-11-02 00:03:28 +0530

    assignment

---
+++ practce.js
@@ -0,0 +1,20 @@
+
+// let a = {
+//     name: "mohit"
+// }
+// console.log(JSON.stringify(a))  // opposite to parser 
+
+
+
+// Difference between Let and Var.
+
+var x = 20
+
+if(x==20){
+    var b = 30
+    // console.log(b)
+    console.log(x)
+
+}
+
+console.log(b)
