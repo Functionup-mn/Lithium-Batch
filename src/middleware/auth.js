5310521da0d1e711acb26fe0b2858a1ebc63538c
Functionup-mn <nadeem2mau@gmail.com>
2022-11-08 22:20:51 +0530

    assignment

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,18 @@
-const authenticate = function(req, req, next) {
+const jwt = require('jsonwebtoken')
+
+const authenticate = function(req, res, next) {
     //check the token in request header
     //validate this token
+    let token = req.headers["x-Auth-token"];
+  if (!token) token = req.headers["x-auth-token"];
+
+  if (!token) return res.send({ status: false, msg: "token must be present in headers" });
+
+  console.log(token);
+
+  let decodedToken = jwt.verify(token, "functionup-Lithium-very-very-secret-key");
+  if (!decodedToken)
+    return res.send({ status: false, msg: "token is invalid" });
 
     next()
 }
@@ -8,5 +20,11 @@ const authenticate = function(req, req, next) {
 
 const authorise = function(req, res, next) {
     // comapre the logged in user's id and the id in request
+    let decodedToken = jwt.verify(token, "functionup-Lithium-very-very-secret-key");
+  if (!decodedToken)
+    return res.send({ status: false, msg: "token is invalid" });
     next()
 }
+
+module.exports.authenticate = authenticate
+module.exports.authorise = authorise
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
 
+const midd = require("../middleware/auth")
+
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
@@ -11,10 +13,11 @@ router.post("/users", userController.createUser)
 router.post("/login", userController.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
-router.post("/users/:userId/posts", userController.postMessage)
+router.get("/users/:userId", midd.authenticate, midd.authorise, userController.getUserData)
+// router.post("/users/:userId/posts", userController.postMessage)
 
-router.put("/users/:userId", userController.updateUser)
-router.delete('/users/:userId', userController.deleteUser)
+router.put("/users/:userId", midd.authenticate, userController.updateUser)
+// router.delete('/users/:userId', userController.deleteUser)
 
 module.exports = router;
+
