3721e196e71d18da3480a584f8f00c333fe690fd
Functionup-mn <nadeem2mau@gmail.com>
2022-11-23 02:03:38 +0530

    final push

--- controller/collegeController.js
+++ controller/collegeController.js
@@ -19,26 +19,34 @@ const createCollege = async (req, res) => {
 const getColleges = async (req, res) => {
     try {
         let collegeName = req.query.collegeName;
-
         if (!collegeName) {
-            return res.status(400).send({ status: false, message: "Please Enter college Name" });
+            return res.status(400).send({status: false, message: "Please Enter college Name"});
         }
+        function validatename(input){
+            let re = /^[A-Za-z ]+$/
+            return re.test(input)
+        }
+        if(validatename(collegeName)==false) return res.status(400).send({status:false, message:"College Name is Invalid"})
+
+        
+        
 
         if (collegeName) {
-            const isValidName = await collegeModel.findOne({ name: collegeName })
+            const isValidName = await collegeModel.findOne({ name: collegeName})
             if (isValidName == null) {
-                return res.status(400).send({ status: false, message: "invalid college name" })
+                return res.status(400).send({ status: false, message:"invalid college name"})
             }
         }
         const collegeData = await collegeModel.findOne({ name: collegeName })
-        if (collegeData.isDeleted == true) {
-            return res.status(400).send({ status: false, message: "this college data is deleted" })
+        console.log(collegeData)
+        if(collegeData.isDeleted == true) {
+            return res.status(400).send({ status: false, message: "this college data is deleted"})
         }
 
-        const collegeId = collegeData["_id"]
+        const collegeId = collegeData ["_id"]
         const internData = await internModel.find({ collegeId: collegeId })
-        if (internData.length == 0) {
-            return res.status(400).send({ status: false, message: "there is no intern from this college " })
+        if (internData.length ==0) {
+            return res.status(400).send({ status: false, message: "there is no intern from this college "})
         }
 
         let data = {
@@ -47,10 +55,10 @@ const getColleges = async (req, res) => {
             logoLink: collegeData.logoLink,
             interns: internData
         }
-        return res.status(200).send({ status: true, message: data })
+        return res.status(200).send({ status: true, message: data})
 
-
-    } catch (err) {
+       
+    } catch(err) {
         return res.status(500).send({ status: false, message: err.message });
     }
 }
