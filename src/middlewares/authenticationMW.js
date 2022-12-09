93a9684f9bef8d4a08504fd275b4e18138ee222f
Functionup-mn <nadeem2mau@gmail.com>
2022-12-09 22:50:51 +0530

    authorization completed

--- src/middlewares/authenticationMW.js
+++ src/middlewares/authenticationMW.js
@@ -4,118 +4,42 @@ const blogModel = require('../Models/blogModel')
 const objectId=mongoose.Types.ObjectId
 
 const authenticate = function (req, res, next) {
-      try {
-          const token = req.headers["x-api-key"]
+  try {
+      const token = req.headers["x-api-key"]
 
-          if (!token) {
-              res.status(400).send({ msg: "Please set x-api-key header" })
-          }
-
-          jwt.verify(token, "Project1-key",(err,decode) =>{ 
-          if(err){
-              return res.status(400).send({status: false, msg:"Incorrect Token"}) 
-          } 
-          (decode==true)
-            next()
-
-          } )
+      if (!token) {
+          res.status(400).send({ msg: "Please set x-api-key header" })
       }
-      catch (error) {
-          res.status(500).send({ msg: "Authentication failure", msg2: error.message })
-      }
+
+      jwt.verify(token, "Project1-key",(err,decoded) =>{ 
+      if(err){
+          return res.status(401).send({status: false, message: err.message}) 
+      }else{ 
+      req.decoded = decoded
+      return next()
+      } 
+      })
+  }
+  catch (error) {
+      res.status(500).send({ msg: "Authentication failure", msg2: error.message })
+  }
 }
-
-
 
 const authorization = async function (req, res, next) {
-    try {
-        const token = req.headers["x-api-key"]
-        const obj1 = {
-            isDeleted: false,
-            isPublished: false
-          }
-        const blogId = req.params.blogId
-        // if (blogId==""||blogId) {
-        //   if(blogId.length==0 || !objectId.isValid(blogId))
-        //   return res.status(400).send({ msg: "please enter blogId  properly in params" })
-        // }else{
-        //   return res.status(400).send({msg:"you missed enter blogId in params"})
-        // }
-    
-        const {authorId,tags,category,subcategory} = req.query
-        if (category) {
-            obj1.category = category;
-          if(obj1.category==0 || (typeof tags != "string") )
-            return res.status(400).send({ msg: "please enter category" }) 
-          }
-      
-          if (authorId) {
-            obj1.authorId = authorId
-            if (obj1.authorId.length == 0 || !(objectId.isValid(obj1.authorId))) {
-              return res.status(400).send({ msg: "please enter authorid properly" })
-            }
-          }
-          if (tags) {
-            obj1.tags = tags
-            if (obj1.tags.length == 0 || (typeof tags != "string")) {
-              return res.status(400).send({ msg: "please enter tags" })
-            }
-          }
-          if (subcategory) {
-            obj1.subcategory = subcategory
-            if (obj1.subcategory.length==0 || (typeof subcategory != "string")) {
-              return res.status(400).send({ msg: "please enter subcategory" })
-            }
-          }
-      
+   const blogId = req.params.blogId
+   if(!blogId) return res.status(400).send({status: false, message: "please enter blogId in params"})
+   checkData = await blogModel.findById(blogId)
+   if(!checkData){
+    return res.status(400).send({status: false, message: "blog is not found of this blogId"})
+   }
+   console.log(checkData.authorId)
+   let checkAuthorId = req.decoded.authorId
+   if(checkData.authorId.toString() !== checkAuthorId){
+    return res.status(403).send({status: false, message: "user is not authorized"})
+   }
 
-        if (!token) {
-            res.status(400).send({ msg: "Please set x-api-key header" })
-        }
-        let decodedToken = jwt.verify(token, "Project1-key")             
-        if (!decodedToken) {
-            res.status(400).send({ msg: "Enter valid token" })
-        }
-        
-         if(blogId){
-            let result = await blogModel.findById(blogId)
-           
-              console.log(result)
-            if(!result){ res.status(404).send( {msg: "nothing found.."})}
-    
-             console.log(decodedToken.authorId)
-             console.log(result.authorId)
-    
-            if (decodedToken.authorId !== result.authorId.toString()) {  // authorization
-                res.status(401).send({ msg: "User Not authorised" })
-            }
-            else {
-                next()
-            }
-        
-         }else{
-
-          let count=0
-          let result = await blogModel.find({obj1})
-          if(!result.length){res.send("not found")}
-            for(let i=0; i<result.length; i++)
-            {
-            if (decodedToken.authorId === result[i].authorId.toString()) {
-              count++
-               }
-            }
-            if(count==0)
-            {
-              return res.status(401).send({ msg: "User Not authorised" })
-            }
-            next()
-     }
-    }
-    catch (error) {
-        res.status(500).send({ msg: error.message })
-    }
-    
-}
+   next()
+  }
 
 
 
